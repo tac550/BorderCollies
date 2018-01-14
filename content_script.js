1a8c4cebdf51adb93c1fc0c666711d0ababcd45e
TOM-PC\Thomas <tac550@gmail.com>
2018-01-14 17:14:33 -0500

    Update 1.2.0

--- content_script.js
+++ content_script.js
@@ -2,7 +2,7 @@ var allImg = document.getElementsByTagName("img"), i = 0, img;
 
 var images = ["http://www.dogbreedinfo.com/images12/BorderCollie6yearsKaya.JPG",
     "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Border-Collie-3.jpg",
-    "http://nextranks.com/data_images/dogs/border-collie/border-collie-05.jpg",
+    "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Border-Collie-2.jpg",
     "http://www.lockeyebc.com/images/spice_pup_pics/7wks/dollyrainbowwave1.jpg",
     "http://animalsbreeds.com/wp-content/uploads/2015/01/border-collie-10.jpg"];
 
